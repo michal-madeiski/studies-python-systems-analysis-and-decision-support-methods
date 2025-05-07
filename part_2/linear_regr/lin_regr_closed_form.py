abe98d8180eab0c082b5fbe39be36069dacc2712
Michal Madeiski <michal.madeiski@onet.pl>
2025-05-07 20:08:45 +0200

    Clean up code

--- part_2/linear_regr/lin_regr_closed_form.py
+++ part_2/linear_regr/lin_regr_closed_form.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 class LinearRegressionClosedFormula:
     def __init__(self):
         self._a_ = None  #coefficients of x
@@ -10,12 +11,12 @@ class LinearRegressionClosedFormula:
 
         X_with_1 = np.c_[np.ones(X.shape[0]), X] #fill 1st column of X with 1
 
-        #theta = (X^T X)^-1 X^T y
+        #theta = (X^T * X)^-1 * (X^T * y)
         XT = X_with_1.T
-        XTX = np.dot(XT, X_with_1)
+        XTX = XT @ X_with_1
         XTX_inv = np.linalg.pinv(XTX)
-        XTy = np.dot(XT, y)
-        theta = np.dot(XTX_inv, XTy)
+        XTy = XT @ y
+        theta = XTX_inv @ XTy
 
         self._b_ = theta[0]
         self._a_ = theta[1:]
@@ -29,9 +30,9 @@ class LinearRegressionClosedFormula:
         if hasattr(X, "toarray"):
             X = X.toarray()
 
-        return np.dot(X, self._a_) + self._b_
+        return (X @ self._a_) + self._b_
 
-    def score(self, X, y):
+    def r2_score(self, X, y):
         y_pred = self.predict(X)
         square_sum_rest = np.sum((y - y_pred) ** 2)
         square_sum_total = np.sum((y - y.mean()) ** 2)
