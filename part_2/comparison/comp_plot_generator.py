d00c493d41bec999befb6861af1c6fd2e18153a8
Michal Madeiski <michal.madeiski@onet.pl>
2025-06-04 17:41:43 +0200

    Add sample plots to part_1 and sample plots and csv_results to part_2

--- part_2/comparison/comp_plot_generator.py
+++ part_2/comparison/comp_plot_generator.py
@@ -12,11 +12,12 @@ def line_plot(path_to_csv, title, save_title):
     plt.tight_layout()
     plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
     plt.subplots_adjust(right=0.8)
-    plt.savefig(save_title + ".png")
+    plt.savefig("plots/" + save_title + ".png")
     plt.close()
 
 def line_plot_mse(path_to_csv, title, save_title):
     data = pd.read_csv(path_to_csv)
+    data = data.drop(index=1) #drop value_eur to for better plot visualisation
     data_melted = data.melt(id_vars="target", var_name="epoch", value_name="mse")
     plt.figure(figsize=(10, 6))
     sns.lineplot(x="epoch", y="mse", data=data_melted, marker="o", hue="target")
@@ -26,10 +27,10 @@ def line_plot_mse(path_to_csv, title, save_title):
     plt.tight_layout()
     plt.legend(loc="upper left", bbox_to_anchor=(1, 1))
     plt.subplots_adjust(right=0.8)
-    plt.savefig(save_title + ".png")
+    plt.savefig("plots/" + save_title + ".png")
     plt.close()
 
-line_plot("num_comp.csv", "R2 score for selected features", "num_comp_plot")
-line_plot("cat_comp.csv", "Accuracy score for selected features", "cat_comp_plot")
-line_plot_mse("all_mse.csv", "MSE by epoch", "all_mse_plot")
-line_plot_mse("all_mse_poly.csv", "MSE by epoch", "all_mse_poly_plot")
+line_plot("csv_results/num_comp.csv", "R2 score for selected features", "num_comp_plot")
+line_plot("csv_results/cat_comp.csv", "Accuracy score for selected features", "cat_comp_plot")
+line_plot_mse("csv_results/all_mse.csv", "MSE by epoch", "all_mse_plot")
+#line_plot_mse("all_mse_poly.csv", "MSE by epoch", "all_mse_poly_plot")
